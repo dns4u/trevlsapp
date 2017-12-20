6f25c640849deb5932dc5c1fb93b92f3c650f63d
dns4u <dinesh.regmi048@gmail.com>
2017-12-20 21:08:53 +0545

    add user information in review page

--- resources/views/front/home/review.blade.php
+++ resources/views/front/home/review.blade.php
@@ -132,10 +132,104 @@
                                         </p>
                                     </div>
                                     <div class="col-md-6 detail-list"><p class="total">${{$newArray['totalPriceOfProduct']}}</p></div>
-                                    <div class="col-md-12 total-btn">
-                                        <button type="button" id="reserve-btn" class="btn btn-default reserve-btn">RESERVE NOW</button>
-                                        <p>Reserve Now, Pay Later</p>
-                                    </div>
+
+
+                                     <div class="container">
+            <h4>
+                4. Enter your details
+            </h4>
+            <div class="row">
+                <div class="col-md-8">
+                    <form action="{{route('front.product.userdetail.send',$newArray['row']['id'])}}" method="post">
+                        {{csrf_field()}}
+                        <div class="heading-driver">
+                            <div class="row">
+                                <div class="col-md-12"><h3>Passenger Information</h3></div>
+                                <div class="col-md-3"><h4></h4>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label for="firstName">First Name</label>
+                                    <input type="text" name="first_name" value="{{ old('first_name') }}" class="form-control" id="firstName">
+                                    @if ($errors->has('first_name'))
+                                        <span class="help-block errorColor">
+                                        <strong>{{ $errors->first('first_name') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="lasttName">Last Name</label>
+                                    <input type="text" name="last_name" value="{{ old('last_name') }}" class="form-control" id="lastName">
+                                    @if ($errors->has('last_name'))
+                                        <span class="help-block errorColor">
+                                        <strong>{{ $errors->first('last_name') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label for="email">Email Address</label>
+                                    <input type="text" name="email" value="{{ old('email') }}" class="form-control" id="email">
+                                    @if ($errors->has('email'))
+                                        <span class="help-block errorColor">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="PhoneNumber">Phone Number</label>
+                                    <input name="phone" type="text" value="{{ old('phone') }}" class="form-control" id="phoneNumber">
+                                    @if ($errors->has('phone'))
+                                        <span class="help-block errorColor">
+                                        <strong>{{ $errors->first('phone') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <label for="address">Address</label>
+                                    <input type="text" name="address" value="{{ old('address') }}" class="form-control" id="address">
+                                    @if ($errors->has('address'))
+                                        <span class="help-block errorColor">
+                                        <strong>{{ $errors->first('address') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+
+                        <button type="submit" id="reserve-btn" class="btn btn-default reserve-btn">RESERVE NOW</button>
+                    </form>
+                </div>
+                <div class="col-md-4">
+                    <div class="rebel-member">
+                        <img class="img-responsive" src="{{asset('images/rebel/rebel-member.jpg')}}">
+                        <h3>Are you a Rebel Member?</h3>
+                        <p>Join the exclusive loyalty membership card that offers 10% off all resevations for an entire year
+                            and a 15%, discount on our retail merchandise. Add to purchase
+                            for $100 or use Card number in Promo Code to redeem savings.</p>
+
+                        <button type="button" class="btn btn-default reserve-btn">ADD TO CART</button>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+                                    
                                 </div>
                             </div>
                         </div>
@@ -337,7 +431,6 @@
 
                 var host = "{{request()->getSchemeAndHttpHost()}}";
                 $('#reserve-btn').click(function (e) {
-                    e.preventDefault();
                     var newdropoffAddress = $('#newdropoffAddress').val();
                     var newreturnAddress = $('#newreturnAddress').val();
                     var newdatepickerFrom = $('#newdatepickerFrom').val();
@@ -365,18 +458,6 @@
 
                         },
                         dataType: 'json',
-                        success: function (resposne) {
-                            window.location.href = host + '/product/userdetail/' + '{{$newArray['row']['id']}}';
-                        },
-                        error: function (response) {
-                            var errors = response.responseJSON;
-                            $.each(response.responseJSON, function (index, value){
-
-                                $('input[name=' + index + ']').after('<span style="color:red">' + value + '</span>');
-                            })
-                        }
-
-
                     });
 
                 });
