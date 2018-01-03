adcd6ed16071e27d7c3dba719db6f67ed447eb70
dns4u <dinesh.regmi048@gmail.com>
2018-01-04 00:00:00 +0545

    add picked up time

---
+++ database/migrations/2018_01_03_180723_chnage_order_to_add_picked_up_time.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChnageOrderToAddPickedUpTime extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('picked_up_time',100)->default(null);
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
